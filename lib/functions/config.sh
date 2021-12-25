fd77fd9a5e12871413ce6f599b80f469098f0330
KillovSky <55511420+KillovSky@users.noreply.github.com>
2021-12-24 23:04:21 -0300

    Release 3.2.4
    Diversas outras correções, melhorias/comandos novos e outras coisas.
    
    Adições:
    
    - Rastreio de encomendas do correio.
    - Melhorias no "Inverter", "Contar", "Tradutor", "TTS", "Translate" e outros.
    
    Correções:
    - Bug no Play/Video/YT-Search #440
    - Signos #437
    - Guildas #442
    - Poll #444
    - News
    - Outros
    
    Pouco a pouco os bugs estão acabando, um agradecimento a todos que reportaram os bugs, isso acelerou muito a solução dos problemas <3
    
    Essa versão continua sendo utilizável apenas em português, não use nos outros 2 idiomas da Íris até que seja dito sobre a estabilidade.
    
    Mais correções em breve.
    
    ⚡ - We still coding - ⚡

--- lib/functions/config.sh
+++ lib/functions/config.sh
@@ -1,32 +1,30 @@
-#!/bin/sh
-
 ######	######	######	######	######	######	######	######	######	######	#
-#	 Construido por KillovSky para utilização no Projeto Íris					#
+#	 Construído por KillovSky para utilização no Projeto Íris					#
 #	   Página oficial -> https://github.com/KillovSky/Iris						#
 #	 Como faz parte do programa Íris, isso utiliza a licença MIT também			#
 # 				Não remova os créditos e divirta-se!							#
 ######	######	######	######	######	######	######	######	######	######	#
 
+# Pode ser feito em JavaScript, mas resolvi dar utilidade ao meu conhecimento de Shell-Script e claro, ao Git Bash|GOW
 # Sistema de cases, semelhante ao da Íris em JavaScript
 case "$1" in
 
-# Gerador de dados falsos, ele "baixa" a página e usa ferramentas de shell para formatar
-# Pode ser feito em JavaScript, mas resolvi dar utilidade ao meu conhecimento de shell e claro, ao Git Bash
+# Gerador de dados falsos, ele acessa a página e formata ela removendo todo o HTML e deixando apenas as informações uteis
    "dados")
 		data=$(curl -s https://www.invertexto.com/gerador-de-pessoas | egrep -v "<option" | egrep "label|value" | sed 's/<input type="text" class="form-control" value=//g' | sed 's/<label>//g' | sed 's/<\/label>//g' | sed 's/$/\n/g' | sed 's/						//g' | sed 's/>$//g' | egrep -v "class")
 		echo "$data"
-   ;;
-
-# Sistema de noticias, ele baixa a página de Noticias do Google pelo JavaScript, e então as formata usando ferramentas de Shell-Script daqui
-   "news")
-		news=$(cat news.xml | sed 's/>/\n/g' | egrep 'pubDate|/description|/source|/title' | sed 's/<\/title//g' | sed 's/<\/pubDate//g' | sed 's/target\=\"/\ndontuseline/g' | sed 's/<\/source/dontuseline\n/g' | egrep -v 'dontuseline|</description|<pubDate' | sed 's/&lt;a//g' | sed 's/href\=\"//g' | sed 's/\"//g')
-		echo "$news"
    ;;
 
 # Sistema que aleatoriza e adquire apenas uma linha do texto, feito em Shell-Script para evitar sobrecarga em JavaScript
    "line")
 		line=$(shuf -n $2 $3)
 		echo "$line"
+   ;;
+
+# Sistema de horoscopo, ele acessa a página do portal terra e remove o HTML não usado, separando apenas o texto de signo da página
+   "signo")
+		signo=$(curl -s https://www.terra.com.br/vida-e-estilo/horoscopo/signos/$2 | egrep 'article_embed_subtitle|<p class="text"' | sed 's/<p class="text">/\n/g' | sed 's/<\/p><h2 class="text">/\n\n/g' | sed 's/<\/h2>/\n/g' | sed 's/<\/p>/\n/g' | sed 's/        <h3 class="article_embed_subtitle">//g' | sed 's/<\/h3>//g' | sed 's/        <p class="text" style="text-align: justify;">//g' | sed 's/        //g' | sed 's/<\/strong>//g' | sed 's/<strong>//g' | sed 's/<h2>//g')
+		echo "$signo"
    ;;
 
 # Aqui é onde se fecha o script, pode ser fechado usando a palavra ao contrario, "case -> esac", "if" -> "fi".
