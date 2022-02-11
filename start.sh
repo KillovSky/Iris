96bcc0c63c5516885c771ba8ec79ddf18e71edeb
gabrieldiaspereira <gabrieldiaspmg@gmail.com>
2022-02-11 09:44:07 -0300

    add start.sh for bug prevention

---
+++ start.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+GREEN='\033[0;32m'
+while : 
+do
+echo "${GREEN} INICIANDO A ÍRIS...
+ESTE ARQUIVO É PROGRAMADO PARA O BOT NÃO CAIR
+APÓS ALGUMA QUEDA DE CONEXÃO"
+    npm start
+    sleep 1
+
+done
