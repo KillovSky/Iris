573513c0474d1334c9976c4987a98220219867cb
KillovSky <55511420+KillovSky@users.noreply.github.com>
2024-12-16 16:55:49 -0300

    Egg PRÉ-Release 16/12/24 [BETA]
    **"Correções e melhorias diversas:**
    
    - **Fix no youtube-dl-exec** para corrigir a versão anterior que a Íris estava usando.
    - Ajustes de correção do **SQL** para caso a DB sofra alguma falha.
    - Adição do **loop `for` na função `construct`** para garantir que nenhuma mensagem seja ignorada.
    - Adição de **JSDoc** para documentar o código relacionado ao jogo de xadrez.
    - **Melhorias no uso de idiomas** das mensagens no comando chat, permitindo escolher o idioma da resposta.
    - Remover de um parceiro do site devido ao encerramento das operações dele.

--- lib/Functions/SQL/Global/custom.sql
+++ lib/Functions/SQL/Global/custom.sql
@@ -1,3 +1,41 @@
+-- Cria a tabela banks
+CREATE TABLE IF NOT EXISTS banks (
+    created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    id TEXT NOT NULL PRIMARY KEY,
+    data JSONB NOT NULL,
+    UNIQUE(id)
+);
+
+-- Cria a tabela groups
+CREATE TABLE IF NOT EXISTS groups (
+    created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    id TEXT NOT NULL PRIMARY KEY,
+    data JSONB NOT NULL,
+    UNIQUE(id)
+);
+
+-- Cria a tabela leveling
+CREATE TABLE IF NOT EXISTS leveling (
+    id INTEGER PRIMARY KEY,
+    created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    user TEXT NOT NULL,
+    chat TEXT NOT NULL,
+    data JSONB NOT NULL,
+    UNIQUE(user, chat) ON CONFLICT IGNORE
+);
+
+-- Cria a tabela personal
+CREATE TABLE IF NOT EXISTS personal (
+    created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    id TEXT NOT NULL PRIMARY KEY,
+    data JSONB NOT NULL,
+    UNIQUE(id)
+);
+
 -- Para a tabela leveling
 UPDATE leveling
 SET modified = CURRENT_TIMESTAMP
